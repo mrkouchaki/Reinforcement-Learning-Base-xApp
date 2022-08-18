66744cd04a5ed62a53e7fa3dba497068ba0fa52e
mrkouchaki <93693562+mrkouchaki@users.noreply.github.com>
2022-08-18 11:38:29 -0500

    Update populate.py

--- mr/populate.py
+++ mr/populate.py
@@ -106,90 +106,90 @@ class INSERTDATA:
         self.client.query('DROP MEASUREMENT '+measname)
         print('elf.client.query(DROP MEASUREMENT +measname)=', elf.client.query('DROP MEASUREMENT '+measname))
 
-def time(df):
-    print('///////////////enter def time//////////////')
-    df.index = pd.date_range(start=datetime.datetime.now(), freq='10ms', periods=len(df))
-    #df.index = pd.to_numeric(pd.date_range(start=datetime.datetime.now(), freq='10ms', periods=len(df)))
-    print('df.index=',df.index)
-    print(df)
-    print('df[0]=', df[0])
-    print('df[35]=', df[35])
-    print('df[36]=', df[36])
-    #print(df['state'])
-    #print('lambda x: str(x)=', lambda x: str(x))
-    #df['state'] = df['state'].apply(lambda x: str(x))
-    df[0] = df[0].apply(lambda x: str(x))
-    print('df=', df)
-    print('df[0]=', df[0])
-    print('df[35]=', df[35])
-    print('df[36]=', df[36])
-    return df
+# def time(df):
+#     print('///////////////enter def time//////////////')
+#     df.index = pd.date_range(start=datetime.datetime.now(), freq='10ms', periods=len(df))
+#     #df.index = pd.to_numeric(pd.date_range(start=datetime.datetime.now(), freq='10ms', periods=len(df)))
+#     print('df.index=',df.index)
+#     print(df)
+#     print('df[0]=', df[0])
+#     print('df[35]=', df[35])
+#     print('df[36]=', df[36])
+#     #print(df['state'])
+#     #print('lambda x: str(x)=', lambda x: str(x))
+#     #df['state'] = df['state'].apply(lambda x: str(x))
+#     df[0] = df[0].apply(lambda x: str(x))
+#     print('df=', df)
+#     print('df[0]=', df[0])
+#     print('df[35]=', df[35])
+#     print('df[36]=', df[36])
+#     return df
 
-def populatedb_T0():
-    print('/////////////enter def populatedb_T0()///////////')
-    #data = pd.read_csv('C:/Users/Mohammadreza/Desktop/My Class/Proj-DC/My Works/Scheduling/xApp/mr7-main/mr/cells.csv')
+# def populatedb_T0():
+#     print('/////////////enter def populatedb_T0()///////////')
+#     #data = pd.read_csv('C:/Users/Mohammadreza/Desktop/My Class/Proj-DC/My Works/Scheduling/xApp/mr7-main/mr/cells.csv')
     
-    data_T0 = env.reset()
-    print('data_T0 = env.reset()=', data_T0)
+#     data_T0 = env.reset()
+#     print('data_T0 = env.reset()=', data_T0)
 
-    data_T0 = pd.DataFrame(data_T0)
-    print('data_T0 = pd.DataFrame(data_T0)=',data_T0)
+#     data_T0 = pd.DataFrame(data_T0)
+#     print('data_T0 = pd.DataFrame(data_T0)=',data_T0)
 
-    data_T0 = data_T0.T
-    print('data_T0 = data_T0.T=', data_T0)
+#     data_T0 = data_T0.T
+#     print('data_T0 = data_T0.T=', data_T0)
     
-    data = time(data_T0)
-    print('data= time(data_T0)=',data)
+#     data = time(data_T0)
+#     print('data= time(data_T0)=',data)
     
-    # inintiate connection and create database UEDATA
-    db = INSERTDATA()
-    print('insert data finished, go to write_point')
-    db
-    print('db =', db)
-    db.client.write_points(data, 'RANMeas_T0')
-    print('db.client.write_points(data, RANMeas_T0)=', db.client.write_points(data, 'RANMeas_T0'))
+#     # inintiate connection and create database UEDATA
+#     db = INSERTDATA()
+#     print('insert data finished, go to write_point')
+#     db
+#     print('db =', db)
+#     db.client.write_points(data, 'RANMeas_T0')
+#     print('db.client.write_points(data, RANMeas_T0)=', db.client.write_points(data, 'RANMeas_T0'))
   
-    del data
+#     del data
     
-def populatedb(action):
-    print('/////////////enter def populatedb()///////////')
-    #data = pd.read_csv('C:/Users/Mohammadreza/Desktop/My Class/Proj-DC/My Works/Scheduling/xApp/mr7-main/mr/cells.csv')
+# def populatedb(action):
+#     print('/////////////enter def populatedb()///////////')
+#     #data = pd.read_csv('C:/Users/Mohammadreza/Desktop/My Class/Proj-DC/My Works/Scheduling/xApp/mr7-main/mr/cells.csv')
     
-    env.reset()
-    #action = env.action_space.sample()
-    obs, reward, done, info =env.step(action)
-    print('obs=', obs)
-    print('reward=', reward)
-    print('done=', done)
-    print('info=', info)
+#     env.reset()
+#     #action = env.action_space.sample()
+#     obs, reward, done, info =env.step(action)
+#     print('obs=', obs)
+#     print('reward=', reward)
+#     print('done=', done)
+#     print('info=', info)
     
-    obs = obs.tolist()
-    print('obs = obs.tolist()=', obs)
-    data = obs
-    print('data=obs=', data)
-    data.append(reward)
-    print('data.append(reward)=', data)
-    data.append(done)
-    print('data.append(done)=', data)
+#     obs = obs.tolist()
+#     print('obs = obs.tolist()=', obs)
+#     data = obs
+#     print('data=obs=', data)
+#     data.append(reward)
+#     print('data.append(reward)=', data)
+#     data.append(done)
+#     print('data.append(done)=', data)
 
-    data = pd.DataFrame(data)
-    print('data = pd.DataFrame(data)=',data)
+#     data = pd.DataFrame(data)
+#     print('data = pd.DataFrame(data)=',data)
 
-    data = data.T
-    print('data = data.T=', data)
+#     data = data.T
+#     print('data = data.T=', data)
     
-    data = time(data)
-    print('data= time(data)=',data)
+#     data = time(data)
+#     print('data= time(data)=',data)
     
-    # inintiate connection and create database UEDATA
-    db = INSERTDATA()
-    print('insert data finished, go to write_point')
-    db
-    print('db =', db)
-    db.client.write_points(data, 'RANMeas')
-    print('db.client.write_points(data, RANMeas)=', db.client.write_points(data, 'RANMeas'))
+#     # inintiate connection and create database UEDATA
+#     db = INSERTDATA()
+#     print('insert data finished, go to write_point')
+#     db
+#     print('db =', db)
+#     db.client.write_points(data, 'RANMeas')
+#     print('db.client.write_points(data, RANMeas)=', db.client.write_points(data, 'RANMeas'))
   
-    del data
+#     del data
     
 
     
